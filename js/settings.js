02a02f581377c01d1bbf6ba9e2d166d6030bd009
zekejenkins <145144775+zekejenkins@users.noreply.github.com>
2024-03-18 15:30:50 -0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -3,17 +3,20 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-var dependencySelector = document.getElementById('Dependency');
+var fruitSelector = document.getElementById('fruit');
 var vegetableSelector = document.getElementById('vegetable');
 
 t.render(function(){
   return Promise.all([
-    t.get('board', 'shared', 'Dependency'),
+    t.get('board', 'shared', 'fruit'),
     t.get('board', 'private', 'vegetable')
   ])
-  .spread(function(savedDependency){
-    if(savedFruit && /[a-z]+/.test(savedDependency)){
-      dependencySelector.value = savedDependcy;
+  .spread(function(savedFruit, savedVegetable){
+    if(savedFruit && /[a-z]+/.test(savedFruit)){
+      fruitSelector.value = savedFruit;
+    }
+    if(savedVegetable && /[a-z]+/.test(savedVegetable)){
+      vegetableSelector.value = savedVegetable;
     }
   })
   .then(function(){
@@ -23,9 +26,9 @@ t.render(function(){
 });
 
 document.getElementById('save').addEventListener('click', function(){
-  return t.set('board', 'private', 'Dependency', dependencySelector.value)
+  return t.set('board', 'private', 'vegetable', vegetableSelector.value)
   .then(function(){
-    return t.set('board', 'shared', 'vegetable', vegetableSelector.value);
+    return t.set('board', 'shared', 'fruit', fruitSelector.value);
   })
   .then(function(){
     t.closePopup();
