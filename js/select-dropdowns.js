3c75f51ebc315e2b972bfbab969ac93fe25c6bbc
zekejenkins <145144775+zekejenkins@users.noreply.github.com>
2024-03-18 18:18:17 -0400

    Update select-dropdowns.js

--- js/select-dropdowns.js
+++ js/select-dropdowns.js
@@ -2,6 +2,28 @@
 
 var t = TrelloPowerUp.iframe();
 
+// This part is responsible for initializing the dropdowns with saved values
+t.render(function() {
+  return t.get('board', 'shared', 'selectedFruit')
+    .then(function(savedFruit) {
+      if(savedFruit) {
+        document.getElementById('fruit').value = savedFruit;
+      }
+    })
+    .then(function() {
+      return t.get('board', 'shared', 'selectedVegetable');
+    })
+    .then(function(savedVegetable) {
+      if(savedVegetable) {
+        document.getElementById('vegetable').value = savedVegetable;
+      }
+    })
+    .then(function(){
+      t.sizeTo('#content').done();
+    });
+});
+
+// This part listens for the save button click to store the selections
 document.getElementById('save').addEventListener('click', function(){
   var fruit = document.getElementById('fruit').value;
   var vegetable = document.getElementById('vegetable').value;
