7223c5ecf1e7988b9aef383904a1c1bf0f62bd0c
zekejenkins <145144775+zekejenkins@users.noreply.github.com>
2024-03-19 14:35:19 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -90,19 +90,19 @@ var getBadges = function(t) {
     var badges = [];
     
     // Handle 'independent'
-    if (dependencyType === 'dependent') {
+    if (dependencyType === 'independent') {
       badges.push({
         icon: GRAY_ICON,
-        text: 'Test Dependent',
+        text: 'Independent',
         color: 'green',
       });
     }
     
     // Handle 'dependent'
-    if (dependencyType === 'independent') {
+    if (dependencyType === 'dependent') {
       badges.push({
         icon: GRAY_ICON,
-        text: 'Test Independent',
+        text: 'Dependent' + independentCardName,
         color: 'red',
       });
       
