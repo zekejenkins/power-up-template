91fa6b02fa034f449b9c23d23bd5b79e449bcb1b
zekejenkins <145144775+zekejenkins@users.noreply.github.com>
2024-03-19 13:31:02 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -78,62 +78,45 @@ t.getAll();
 
 var GLITCH_ICON = './images/glitch.svg';
 var WHITE_ICON = './images/icon-white.svg';
-var GRAY_ICON = './images/icon-gray.svg';
-
-var randomBadgeColor = function() {
-  return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
-};
+var GRAY_ICON = './images/icon-gray.svg'; // Ensure this path is correct
 
 var getBadges = function(t){
-  return t.card('name')
-  .get('name')
-  .then(function(cardName){
-    console.log('We just loaded the card name for fun: ' + cardName);
+  return Promise.all([
+    t.get('card', 'shared', 'dependencyType'),
+    t.get('card', 'shared', 'independentCardId')
+  ]).spread(function(dependencyType, independentCardId){
+    var badges = [];
     
-    return [{
-      // dynamic badges can have their function rerun after a set number
-      // of seconds defined by refresh. Minimum of 10 seconds.
-      dynamic: function(){
-        // we could also return a Promise that resolves to this as well if we needed to do something async first
-        return {
-          title: 'Detail Badge', // for detail badges only
-          text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-          icon: GRAY_ICON, // for card front badges only
-          color: randomBadgeColor(),
-          refresh: 10 // in seconds
-        };
+    if(dependencyType === 'independent'){
+      badges.push({
+        icon: GRAY_ICON,
+        text: 'Independent',
+        color: 'green',
+      });
+    } else if(dependencyType === 'dependent'){
+      badges.push({
+        icon: GRAY_ICON,
+        text: 'Dependent',
+        color: 'red',
+      });
+
+      // Fetch the name of the independent card this card is dependent on
+      if(independentCardId){
+        return t.card(independentCardId)
+          .get('name')
+          .then(function(independentCardName){
+            badges.push({
+              icon: GRAY_ICON,
+              text: 'Parent: ' + independentCardName,
+              color: 'blue',
+            });
+            return badges;
+          });
       }
-    }, {
-      // its best to use static badges unless you need your badges to refresh
-      // you can mix and match between static and dynamic
-      title: 'Detail Badge', // for detail badges only
-      text: 'Static',
-      icon: GRAY_ICON, // for card front badges only
-      color: null
-    }, {
-      // card detail badges (those that appear on the back of cards)
-      // also support callback functions so that you can open for example
-      // open a popup on click
-      title: 'Popup Detail Badge', // for detail badges only
-      text: 'Popup',
-      icon: GRAY_ICON, // for card front badges only
-      callback: function(context) { // function to run on click
-        return context.popup({
-          title: 'Card Detail Badge Popup',
-          url: './settings.html',
-          height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-        });
-      }
-    }, {
-      // or for simpler use cases you can also provide a url
-      // when the user clicks on the card detail badge they will
-      // go to a new tab at that url
-      title: 'URL Detail Badge', // for detail badges only
-      text: 'URL',
-      icon: GRAY_ICON, // for card front badges only
-      url: 'https://trello.com/home',
-      target: 'Trello Landing Page' // optional target for above url
-    }];
+    }
+    
+    // Return badges directly if not dependent or no parent card ID is found
+    return badges;
   });
 };
 
